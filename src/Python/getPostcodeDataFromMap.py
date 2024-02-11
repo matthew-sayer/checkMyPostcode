388edbd56d981d2d94e7bda16cb19407c5d2b069
matthew.sayer <matthew.sayer1@gmail.com>
2024-02-11 18:50:55 +0000

    Refactored location of method to retrieve the decile data based on the row that the postcode exists on within the mapping file

--- src/Python/getPostcodeDataFromMap.py
+++
@@ -1,7 +0,0 @@
-import pandas
-class retrieveDataByPostcode:
-    def __init__(self, postcodeMap):
-        self.postcodeMap = postcodeMap
-    def getPostcodeDataFromMap(self, postcode):
-        decileData = self.postcodeMap.loc[self.postcodeMap['PCD'] == postcode]
-        return decileData
