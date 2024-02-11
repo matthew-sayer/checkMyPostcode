fad8bec3f094c64bb9a4519eaad5fffb62238c1d
matthew.sayer <matthew.sayer1@gmail.com>
2024-02-11 18:48:16 +0000

    Refactored application to remove decile data retrieval from Main, to prepare for front-end to be built

---
+++ src/Python/getDecileDataByPostcode.py
@@ -0,0 +1,45 @@
+import pandas as pd
+#Here, I am creating a class to pull in the postcode map. Then, I take the postcode input and check it against the map.
+#After that, I check if it matches and if it does not then I reformat it.
+#If it still doesn't match, I state that the postcode was not found.
+#The checkDecileData function will recursively call itself to reformat the postcode
+#It will do this until it finds a match, or it still can't find one due to no valid postcode being entered
+
+class getDecileData: 
+    def __init__(self, postcodeMapRef):
+        self.postcodeMap = postcodeMapRef
+class takePostcodeInput(getDecileData):
+    def __init__(self, postcodeMapRef):
+        getDecileData.__init__(self, postcodeMapRef)
+
+    def getInput(self):
+        postcode = input('Enter a postcode: ')
+        postcode = postcode.replace(' ', '')
+        postcode = postcode.upper()
+        return postcode
+
+class getDecileDataForPostcode(getDecileData):
+    def __init__(self, postcodeMapRef):
+        getDecileData.__init__(self, postcodeMapRef)
+    def getPostcodeDataFromMap(self, postcode):
+        decileData = self.postcodeMap.loc[self.postcodeMap['PCD'] == postcode]
+        return decileData
+    
+    def checkDecileData(self, postcode):
+        decileData = self.getPostcodeDataFromMap(postcode)
+        if decileData.empty:
+            if len(postcode) == 7:
+                postcode = postcode[:4] + ' ' + postcode[4:]
+                postcode = postcode.upper()
+            elif len(postcode) == 6:
+                postcode = postcode[:3] + ' ' + postcode[3:]
+                postcode = postcode.upper()
+            return self.checkDecileData(postcode)
+        else:
+             return decileData
+
+    def outputDecileData(self, decileData):
+            if not decileData.empty:
+                print(decileData)
+            else: 
+                print('Postcode not found')
--- src/Python/main.py
+++ src/Python/main.py
@@ -1,41 +1,26 @@
 import ingestData as ingest
 import createPostcodeMap as map
-import getPostcodeDataFromMap as getData
+import getDecileDataByPostcode as getDecileData
 import visualiseData as vis
 
 def main(centroidsFile, iodFile):
         print("Application loading...")
+        print("Loading data...")
         ingestData = ingest.IngestData(centroidsFile, iodFile)
         centroidsDF = ingestData.readCentroidsMap()
         iodDF = ingestData.readIndicesOfDeprivation()
+        print("Mapping LSOA data to postcodes...")
         map_data = map.getPostcodeData(centroidsDF, iodDF)
-        postcodeMap = map_data.createPostcodeMap()
-        vis_data = vis.visualiseData(postcodeMap)
-        vis_data.choroplethMap()
-        get_data = getData.retrieveDataByPostcode(postcodeMap)
-
-        while True:
-       #get postcode input 
-                postcode = input('Enter a postcode (or type "quit" to exit): ')
-                #remove postcode spaces
-                postcode = postcode.replace(' ', '')
-                postcode = postcode.upper()
-                if postcode.lower() == 'quit':
-                        break
-                decileData = get_data.getPostcodeDataFromMap(postcode)
-                if decileData.empty:
-                        if postcode.__len__() == 7:
-                              #add space after 4th character
-                                postcode = postcode[:4] + ' ' + postcode[4:]
-                                postcode = postcode.upper()
-                        elif postcode.__len__() == 6:
-                                postcode = postcode[:3] + ' ' + postcode[3:]
-                                postcode = postcode.upper()
-                decileData = get_data.getPostcodeDataFromMap(postcode)
-                if not decileData.empty:
-                        print(decileData)
-                else: 
-                        print('Postcode not found')
+        postcodeMapRef = map_data.createPostcodeMap()
+       #print("Visualising data...")
+        #vis_data = vis.visualiseData(postcodeMap)
+        #vis_data.choroplethMap()
+        #initialise getDecileData class
+        postcodeInput = getDecileData.takePostcodeInput(postcodeMapRef)
+        postcode = postcodeInput.getInput()
+        decileDataInstance = getDecileData.getDecileDataForPostcode(postcodeMapRef)
+        decileData = decileDataInstance.checkDecileData(postcode)
+        decileDataInstance.outputDecileData(decileData)
 
 if __name__ == "__main__":
     main('C:\\Users\\matth\\MyScripts\\temp\\ONSPD_Centroids.csv.gz', 'C:\\Users\\matth\\MyScripts\\temp\\IoD2019.csv.gz')
